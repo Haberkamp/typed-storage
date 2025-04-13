cfa8b174ac5786a260986f6e26292e2cd35be728
Nils Haberkamp <haberkamp.n@gmail.com>
2025-04-13 10:33:51 +0200

    build(@typed-storage/react): fix type error in eslint config

--- packages/react/eslint.config.mjs
+++ packages/react/eslint.config.mjs
@@ -3,6 +3,7 @@
 import eslint from "@eslint/js"
 import tseslint from "typescript-eslint"
 
+/** @type {import('@typescript-eslint/utils').TSESLint.FlatConfig.Config[]} */
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommendedTypeChecked,
