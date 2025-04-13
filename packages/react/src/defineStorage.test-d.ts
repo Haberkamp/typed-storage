5223d0fbd33aa582b114c7db14d4df653a3a2ea9
Nils Haberkamp <haberkamp.n@gmail.com>
2025-04-13 10:19:27 +0200

    test(@typed-storage/react): use typecheck of vitest to test hook API

---
+++ packages/react/src/defineStorage.test-d.ts
@@ -0,0 +1,38 @@
+import { z } from "zod"
+import { defineStorage } from "./defineStorage"
+import { test, assertType } from "vitest"
+
+test("it's not possible to insert a value that violates the schema", () => {
+  // ARRANGE
+  const { useStorage } = defineStorage({
+    someKey: z.string(),
+  })
+
+  const [, setValue] = useStorage("someKey")
+
+  // ACT & ASSERT
+  // @ts-expect-error -- Cannot assign value that violates the schema
+  assertType(setValue(1))
+})
+
+test("it's not possible to access a non string key", () => {
+  // ARRANGE
+  const { useStorage } = defineStorage({
+    someKey: z.string(),
+  })
+
+  // ACT & ASSERT
+  // @ts-expect-error -- Cannot access a non string key
+  assertType(useStorage(1))
+})
+
+test("it's not possible to access a non existent key", () => {
+  // ARRANGE
+  const { useStorage } = defineStorage({
+    someKey: z.string(),
+  })
+
+  // ACT & ASSERT
+  // @ts-expect-error -- Cannot access a non existent key
+  assertType(useStorage("some-non-existent-key"))
+})
