0a392bacd076c19575b5d87291430f288fbce960
Nils Haberkamp <haberkamp.n@gmail.com>
2025-04-13 10:48:04 +0200

    build(@typed-storage/react): configure eslint-plugin-unicorn

--- packages/react/eslint.config.mjs
+++ packages/react/eslint.config.mjs
@@ -2,10 +2,12 @@
 
 import eslint from "@eslint/js"
 import tseslint from "typescript-eslint"
+import eslintPluginUnicorn from "eslint-plugin-unicorn"
 
 /** @type {import('@typescript-eslint/utils').TSESLint.FlatConfig.Config[]} */
 export default tseslint.config(
   eslint.configs.recommended,
+  eslintPluginUnicorn.configs.recommended,
   tseslint.configs.recommendedTypeChecked,
   {
     languageOptions: {
